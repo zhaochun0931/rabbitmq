18fd5ad9a2b32ff483ef394f3b943cbb27fc0ec4
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-08-10 09:35:34 +0800

    Update 7-rabbitmq-cluster-operator.sh

--- install/7-rabbitmq-cluster-operator.sh
+++ install/7-rabbitmq-cluster-operator.sh
@@ -1,1 +1,5 @@
 kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
+
+
+kubectl get customresourcedefinitions.apiextensions.k8s.io | grep rabbitmqclusters.rabbitmq.com
+
