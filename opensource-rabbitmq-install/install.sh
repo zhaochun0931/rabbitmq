e09f05bd64a2cf96c20b8142bc72fabf03d25f7e
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-08-16 09:07:57 +0800

    Update install.sh

--- opensource-rabbitmq-install/install.sh
+++ opensource-rabbitmq-install/install.sh
@@ -3,6 +3,15 @@ add-apt-repository ppa:rabbitmq/rabbitmq-erlang
 apt update
 apt install erlang -y
 
+# check erlang version
+erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' -noshell
+
+
+
+
+
+
+
 
 # install the default erlang
 apt update
