5831ea050a8cfe8d9d0f4c40405b12e7bf45f53f
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-08-18 09:11:32 +0800

    Update Send.java

--- java-client/Send.java
+++ java-client/Send.java
@@ -8,15 +8,20 @@ public class Send {
 
     private final static String QUEUE_NAME = "hello";
 
+
     public static void main(String[] argv) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-            System.out.println(" [x] Sent '" + message + "'");
+        
+        for(int i=1;i<1000;i++){
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            try (Connection connection = factory.newConnection();
+                 Channel channel = connection.createChannel()) {
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                String message = "Hello World!";
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+                System.out.println(" [x] Sent '" + message + "'" + "   " + i);
+            }
         }
+
     }
 }
