3d1fdb7a6da7cc9c90398cbfd995491dd67bbe50
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-08-18 08:25:32 +0800

    Update and rename send.java to Send.java

--- java-client/send.java
+++
@@ -1,3 +0,0 @@
-https://repo1.maven.org/maven2/com/rabbitmq/amqp-client/5.16.0/amqp-client-5.16.0.jar
-https://repo1.maven.org/maven2/org/slf4j/slf4j-api/1.7.36/slf4j-api-1.7.36.jar
-https://repo1.maven.org/maven2/org/slf4j/slf4j-simple/1.7.36/slf4j-simple-1.7.36.jar
