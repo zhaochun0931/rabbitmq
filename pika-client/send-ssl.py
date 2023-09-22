6d647904dfb7a77951712f9a9e26fc7c119b680c
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-09-22 10:24:30 +0800

    Create send-ssl.py

---
+++ pika-client/send-ssl.py
@@ -0,0 +1,37 @@
+import logging
+import pika
+import ssl
+
+
+username = 'admin'
+password = 'password'
+rabbitmq_password  = pika.PlainCredentials(username,password)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+rabbitmq_context = ssl.create_default_context(cafile="/Users/Downloads/rootCA.crt")
+rabbitmq_context.verify_mode = ssl.CERT_REQUIRED
+# context.verify_mode = ssl.CERT_OPTIONAL
+# context.verify_mode = ssl.CERT_NONE
+
+rabbitmq_context.load_cert_chain("/Users/Downloads//mydomain.crt", "/Users/Downloads/mydomain.key")
+rabbitmq_ssl_options = pika.SSLOptions(rabbitmq_context, "www.xiaoming.com")
+
+
+rabbitmq_conn = pika.ConnectionParameters(
+    port=5671,
+    credentials=rabbitmq_password,
+    ssl_options=rabbitmq_ssl_options,
+     )
+
+with pika.BlockingConnection(rabbitmq_conn) as conn:
+    ch = conn.channel()
+    ch.queue_declare(queue='foobar',durable=True)
+    # chan.basic_publish(exchange='',
+    #                    routing_key='qq2',
+    #                    body='hello world!' + dt_string,mandatory=True)
+
+    for i in range(100):
+        ch.basic_publish("", "foobar", "Hello, world!")
+    # print(ch.basic_get("foobar"))
