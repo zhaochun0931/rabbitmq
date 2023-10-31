ec27c8d48b0c33e48dce536d2963d3389e8fd03a
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-10-31 09:31:51 +0800

    Create App.java

---
+++ java-client-ssl/App.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.security.*;
+
+import com.rabbitmq.client.*;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5671);
+
+        factory.useSslProtocol();
+        // Tells the library to setup the default Key and Trust managers for you
+        // which do not do any form of remote server trust verification
+
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+
+        // non-durable, exclusive, auto-delete queue
+        channel.queueDeclare("rabbitmq-java-test", false, true, true, null);
+        channel.basicPublish("", "rabbitmq-java-test", null, "Hello, World".getBytes());
+
+        GetResponse chResponse = channel.basicGet("rabbitmq-java-test", false);
+        if (chResponse == null) {
+            System.out.println("No message retrieved");
+        } else {
+            byte[] body = chResponse.getBody();
+            System.out.println("Received: " + new String(body));
+        }
+
+        channel.close();
+        conn.close();
+    }
+}
