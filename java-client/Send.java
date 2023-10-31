646173d367f74460a32a80f8dce2aee69cf70354
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-10-31 09:16:15 +0800

    Update Send.java

--- java-client/Send.java
+++ java-client/Send.java
@@ -1,27 +1,32 @@
+//package com.mycompany.app;
+
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.nio.charset.StandardCharsets;
 
-public class Send {
 
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
     private final static String QUEUE_NAME = "hello";
 
-
     public static void main(String[] argv) throws Exception {
-        
-        for(int i=1;i<=1000;i++){
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            try (Connection connection = factory.newConnection();
-                 Channel channel = connection.createChannel()) {
-                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-                String message = "Hello World!";
-                channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-                System.out.println(" [x] Sent '" + message + "'" + "   " + i);
-            }
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("admin");
+        factory.setPassword("password");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
         }
-
     }
 }
