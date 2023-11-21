e9aef1f4a95c5233e2d0583250440b9dcdb3c643
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-11-21 11:59:20 +0800

    Update quick-install.sh

--- vmware-rabbitmq-install/quick-install.sh
+++ vmware-rabbitmq-install/quick-install.sh
@@ -16,3 +16,7 @@ kubectl get packages
 
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 kubectl annotate storageclass local-path storageclass.kubernetes.io/is-default-class=true
+
+
+
+echo -e "\n\n\nInstallation successfully! \nYou can deloy Rabbitmq cluster now."
