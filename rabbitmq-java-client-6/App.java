e6a30b2b0e9c63d582cf350431d92522cbd92f09
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-11-24 15:17:57 +0800

    Create App.java

---
+++ rabbitmq-java-client-6/App.java
@@ -0,0 +1,49 @@
+// declare the quorum queue
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import java.nio.charset.StandardCharsets;
+
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+
+        // 使用ConnectionFactory创建连接
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+// 注意这里写5672，是amqp协议端口
+        factory.setPort(5672);
+        factory.setUsername("default_user_NLv82E0apnOPVrcCM5R");
+        factory.setPassword("_v--3BLf1D-QMzu_ffONO5TbYONCoufs");
+        factory.setVirtualHost("test");
+
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+            Map<String, Object> qtype = new HashMap<>();
+            qtype.put("x-queue-type", "quorum");
+
+
+            for (int i = 2; i <= 6000; i++) {
+                System.out.println("hello");
+                channel.queueDeclare("qq" +i , true, false, false, qtype);
+
+
+            }
+
+//            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+//            String message = "Hello World!";
+//            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+//            System.out.println(" [x] Sent '" + message + "'");
+        }
+
+    }
+}
