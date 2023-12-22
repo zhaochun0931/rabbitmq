5339f3cccfd597c7a283c993907a3965be21cc78
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2023-12-22 15:31:47 +0800

    Create App.java

---
+++ rabbitmq-java-client-ssl/App.java
@@ -0,0 +1,43 @@
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.Security;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // 设置 SSL 环境
+        char[] keystorePassphrase = "password".toCharArray();
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(new FileInputStream("/tmp/tls.jks"), keystorePassphrase);
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, keystorePassphrase);
+
+        char[] truststorePassphrase = "password".toCharArray();
+        KeyStore tks = KeyStore.getInstance("JKS");
+        tks.load(new FileInputStream("/tmp/tls-ts.jks"), truststorePassphrase);
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(tks);
+
+        SSLContext c = SSLContext.getInstance("TLSv1.2");
+        c.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+        // 创建连接
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5671);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        factory.useSslProtocol(c);
+        Connection connection = factory.newConnection();
+        // 进行其他操作
+        // ...
+        // 关闭连接
+        connection.close();
+    }
+}
