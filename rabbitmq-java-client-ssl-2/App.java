f6f186e2fc8fa94e0c8107388024fef38483bc8b
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2023-12-22 15:44:54 +0800

    Update App.java

--- rabbitmq-java-client-ssl-2/App.java
+++ rabbitmq-java-client-ssl-2/App.java
@@ -1,1 +1,54 @@
+import java.io.*;
+import java.security.*;
+import javax.net.ssl.*;
 
+import com.rabbitmq.client.*;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        char[] keyPassphrase = "password".toCharArray();
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+        ks.load(new FileInputStream("/tmp/tls.p12"), keyPassphrase);
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, keyPassphrase);
+
+
+
+
+        char[] trustPassphrase = "password".toCharArray();
+        KeyStore tks = KeyStore.getInstance("JKS");
+        tks.load(new FileInputStream("/tmp/tls-ts.jks"), trustPassphrase);
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(tks);
+
+        SSLContext c = SSLContext.getInstance("TLSv1.2");
+        c.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5671);
+        factory.useSslProtocol(c);
+//        factory.enableHostnameVerification();
+
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+
+        channel.queueDeclare("rabbitmq-java-test", false, true, true, null);
+        channel.basicPublish("", "rabbitmq-java-test", null, "Hello, World".getBytes());
+
+        GetResponse chResponse = channel.basicGet("rabbitmq-java-test", false);
+        if (chResponse == null) {
+            System.out.println("No message retrieved");
+        } else {
+            byte[] body = chResponse.getBody();
+            System.out.println("Received: " + new String(body));
+        }
+
+        channel.close();
+        conn.close();
+    }
+}
