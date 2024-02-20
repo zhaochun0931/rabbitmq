cf10770bed39bbba98a7e7feefd29d6e6a0602bc
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-02-20 14:35:45 +0800

    Update App.java

--- rabbitmq-java-client-0/App.java
+++ rabbitmq-java-client-0/App.java
@@ -1,4 +1,6 @@
-//package com.mycompany.app;
+import java.io.*;
+import java.lang.Thread;
+
 
 
 import com.rabbitmq.client.Channel;
@@ -12,23 +14,31 @@ import java.nio.charset.StandardCharsets;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "qq1";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         factory.setUsername("admin");
         factory.setPassword("password");
-        factory.setVirtualHost("test");
-        
+        factory.setVirtualHost("/");
+
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             String message = "Hello World!";
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
             System.out.println(" [x] Sent '" + message + "'");
+            
+Thread.sleep(10000);
         }
+
+
+
+
+
+
     }
 }
