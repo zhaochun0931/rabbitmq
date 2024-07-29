d517baee013285f15f5f8de45ac72c26b62ccf66
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-07-29 11:55:33 +0800

    Update 01-cluster-essential.sh

--- vmware-rabbitmq-install/01-cluster-essential.sh
+++ vmware-rabbitmq-install/01-cluster-essential.sh
@@ -1,6 +1,3 @@
-
-# MUST CHANGE THE USERNAME AND THE PASSWORD BEFORE INSTALLING THIS  FILE
-# MUST CHANGE THE USERNAME AND THE PASSWORD BEFORE INSTALLING THIS  FILE
 # MUST CHANGE THE USERNAME AND THE PASSWORD BEFORE INSTALLING THIS  FILE
 
 
@@ -12,19 +9,17 @@
 
 
 # v1.9
-export MY_BROADCOM_SUPPORT_ACCESS_TOKEN=password
+export MY_BROADCOM_SUPPORT_ACCESS_TOKEN=API-TOKEN
 export INSTALL_BUNDLE=cluster-essentials.packages.broadcom.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:678c20e14e1065c6a97828632d02b0716ef453e2c9b3c5e1ea0dba1817bd8125
 export INSTALL_REGISTRY_HOSTNAME=cluster-essentials.packages.broadcom.com
 export INSTALL_REGISTRY_USERNAME=xxx@broadcom.com
 export INSTALL_REGISTRY_PASSWORD=${MY_BROADCOM_SUPPORT_ACCESS_TOKEN}
-
 ./install.sh --yes
 
 
 
 
 After installation, you can verify that Tanzu Cluster Essentials is installed correctly by checking the relevant pods are running. Run this command:
-
 kubectl get all -n kapp-controller
 kubectl get all -n secretgen-controller
 
