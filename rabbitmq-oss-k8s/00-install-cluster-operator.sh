a2e0169b7d1169d0f53636cb4cda35009789196c
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-08-09 12:35:05 +0800

    Update 00-install-cluster-operator.sh

--- rabbitmq-oss-k8s/00-install-cluster-operator.sh
+++ rabbitmq-oss-k8s/00-install-cluster-operator.sh
@@ -1,5 +1,14 @@
+# install cluster operator
 kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
-
-
 kubectl get customresourcedefinitions.apiextensions.k8s.io | grep rabbitmqclusters.rabbitmq.com
+
+
+
+
+
+
+
+
+# install message topology operator
+
 
