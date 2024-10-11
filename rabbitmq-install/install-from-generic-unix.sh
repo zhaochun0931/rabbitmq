6587b71ee58846c46f2a9b688fe303cc76d30993
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-10-11 21:32:50 +0800

    Create install-from-generic-unix.sh

---
+++ rabbitmq-install/install-from-generic-unix.sh
@@ -0,0 +1,11 @@
+
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.7/rabbitmq-server-generic-unix-3.13.7.tar.xz
+
+
+
+rabbitmq-server -detached
+
+
+
+
+rabbitmqctl shutdown
