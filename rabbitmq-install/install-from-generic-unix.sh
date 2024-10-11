747e882103775f0ce3ff8cd38efa2944bb7af690
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-10-11 21:46:41 +0800

    Update install-from-generic-unix.sh

--- rabbitmq-install/install-from-generic-unix.sh
+++ rabbitmq-install/install-from-generic-unix.sh
@@ -1,5 +1,20 @@
-
 wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.7/rabbitmq-server-generic-unix-3.13.7.tar.xz
+
+
+
+tar -xf rabbitmq-server-generic-unix-3.13.7.tar.xz
+
+
+cat << done >> ~/.bash_profile
+export PATH=/root/rabbitmq_server-3.13.7/sbin:$PATH
+done
+
+
+
+# run rabbitmq server in the backgound
+rabbitmq-server -detached
+
+
 
 
 
