06cc5b3c377d1e5d4b975336f0bf0f5eab1db759
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-11-04 11:30:21 +0800

    Update install-perftest.sh

--- install-perftest.sh
+++ install-perftest.sh
@@ -10,8 +10,8 @@ wget https://github.com/rabbitmq/rabbitmq-perf-test/releases/download/v2.21.0/pe
 
 # The most basic way of running PerfTest only specifies a URI to connect to, a number of publishers to use (say, 1) and a number of consumers to use (say, 2). 
 java -jar perf-test-2.21.0.jar
-java -jar perf-test-2.21.0.jar --quorum-queue --queue qq1
-
+java -jar perf-test-2.21.0.jar --quorum-queue --queue qq
+java -jar perf-test-2.21.0.jar --stream-queue --queue sq
 
  -x,--producers <arg>                           producer count, default is 1
 
