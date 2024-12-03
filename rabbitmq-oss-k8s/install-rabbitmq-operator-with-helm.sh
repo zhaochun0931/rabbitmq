f730d22048703b06966462ef2c67204469f4c74b
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-12-03 16:40:42 +0800

    Update install-rabbitmq-operator-with-helm.sh

--- rabbitmq-oss-k8s/install-rabbitmq-operator-with-helm.sh
+++ rabbitmq-oss-k8s/install-rabbitmq-operator-with-helm.sh
@@ -1,2 +1,6 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install my-release bitnami/rabbitmq-cluster-operator
+
+
+# The last command deploys the RabbitMQ Cluster Kubernetes Operator on the Kubernetes cluster in the default configuration.
+
