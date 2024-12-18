250c3b684c5030266c346c79e31fa8eb3a9157a7
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-12-18 19:47:10 +0800

    Update and rename rabbitmq-install/install-from-generic-unix.sh to install-rabbitmq-generic-unix.sh

--- install-rabbitmq-generic-unix.sh
+++ install-rabbitmq-generic-unix.sh
@@ -1,8 +1,8 @@
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.7/rabbitmq-server-generic-unix-3.13.7.tar.xz
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v4.0.5/rabbitmq-server-generic-unix-4.0.5.tar.xz
 
 
 
-tar -xf rabbitmq-server-generic-unix-3.13.7.tar.xz
+tar -xf rabbitmq-server-generic-unix-4.0.5.tar.xz
 
 
 cat << done >> ~/.bash_profile
