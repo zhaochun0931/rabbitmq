4b2b54e2671c730cf25148af42b53861f5237f65
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-12-19 08:52:48 +0800

    Delete rabbitmq-pika-client-2/basic-get-2.py

--- rabbitmq-pika-client-2/basic-get-2.py
+++
@@ -1,9 +0,0 @@
-import os
-import pika
-import sys
-
-# this demo will use the basic_get to retrieve message without ACK
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-channel.basic_get(queue='hello')
