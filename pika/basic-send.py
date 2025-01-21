192fa6f489375cec372be3a3c4cbe9690450f985
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-01-21 14:29:05 +0800

    Update basic-send.py

--- pika/basic-send.py
+++ pika/basic-send.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import pika
+import time
+
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
@@ -9,4 +11,10 @@ channel.queue_declare(queue='hello')
 
 channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 print(" [x] Sent 'Hello World!'")
+
+
+# Sleep for 10 seconds
+time.sleep(10)
+
+
 connection.close()
