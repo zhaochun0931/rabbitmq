9cd2d10b01affc0807facb343e269e10b94da294
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-04-01 11:54:59 +0800

    Update install-rabbitmq-docker.sh

--- install-rabbitmq-docker.sh
+++ install-rabbitmq-docker.sh
@@ -1,4 +1,4 @@
-docker run -d -it --rm --name rabbitmq --hostname rabbitmq -p 5672:5672 -p 15672:15672 \
+docker run -d -it --rm --name rabbitmq --hostname rabbitmq -p 5672:5672 -p 15672:15672 -p 1883:1883 \
 -e RABBITMQ_ERLANG_COOKIE=rabbitmq \
 -e RABBITMQ_DEFAULT_USER=admin \
 -e RABBITMQ_DEFAULT_PASS=password \
